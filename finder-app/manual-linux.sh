75f16959f261a15057aa41e7db1097759a2ac60a
xuanlongbui <xuanlong1372000@gmail.com>
2024-10-11 19:29:53 +0700

    full test

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -24,31 +24,31 @@ fi
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
-# if [ ! -d "${OUTDIR}/linux-stable" ]; then
-#     #Clone only if the repository does not exist.
-# 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-# 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
-# fi
-# if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-#     cd linux-stable
-#     echo "Checking out version ${KERNEL_VERSION}"
-#     git checkout ${KERNEL_VERSION}
+if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    #Clone only if the repository does not exist.
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+fi
+if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+    cd linux-stable
+    echo "Checking out version ${KERNEL_VERSION}"
+    git checkout ${KERNEL_VERSION}
 
-#    # TODO: Add your kernel build steps here
-#     # echo a| sudo -S apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
-#     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
-#     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE -j12
-#     echo a| sudo -S cp $OUTDIR/linux-stable/arch/$ARCH/boot/Image $OUTDIR/
-# fi
+   # TODO: Add your kernel build steps here
+    # echo a| sudo -S apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE -j12
+    echo a| sudo -S cp $OUTDIR/linux-stable/arch/$ARCH/boot/Image $OUTDIR/
+fi
 
-# echo "Adding the Image in outdir"
-# echo "Creating the staging directory for the root filesystem"
-# cd "$OUTDIR"
-# if [ -d "${OUTDIR}/rootfs" ]
-# then
-# 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-#     echo a| sudo -S rm  -rf ${OUTDIR}/rootfs
-# fi
+echo "Adding the Image in outdir"
+echo "Creating the staging directory for the root filesystem"
+cd "$OUTDIR"
+if [ -d "${OUTDIR}/rootfs" ]
+then
+	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    echo a| sudo -S rm  -rf ${OUTDIR}/rootfs
+fi
 
 # TODO: Create necessary base directories
 ROOTFS=$OUTDIR/rootfs
@@ -58,29 +58,26 @@ mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 which aarch64-none-linux-gnu-gcc
-# cd "$OUTDIR"
-# if [ ! -d "${OUTDIR}/busybox" ]
-# then
-# git clone git://busybox.net/busybox.git
-#     cd busybox
-#     git checkout ${BUSYBOX_VERSION}
-#     # TODO:  Configure busybox
-#     make distclean
-#     make defconfig
-#     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
-# else
-#     cd busybox
-# fi
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/busybox" ]
+then
+git clone git://busybox.net/busybox.git
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+    # TODO:  Configure busybox
+    make distclean
+    make defconfig
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+else
+    cd busybox
+fi
 
 # TODO: Make and install busybox
-# make CONFIG_PREFIX=$ROOTFS ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install
+make CONFIG_PREFIX=$ROOTFS ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install
 cd $ROOTFS
-# echo "Library dependencies"
-# ${CROSS_COMPILE}readelf -a ${ROOTFS}/bin/busybox | grep "program interpreter"
-# ${CROSS_COMPILE}readelf -a ${ROOTFS}/bin/busybox | grep "Shared library"
-
-# cd /home/aarch64-none-linux-gnu/libc/lib64
-
+echo "Library dependencies"
+${CROSS_COMPILE}readelf -a ${ROOTFS}/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${ROOTFS}/bin/busybox | grep "Shared library"
 
  # TODO: Add library dependencies to rootfs
 
@@ -89,30 +86,30 @@ cp /usr/local/arm-cross-compiler/install/gcc-arm-10.3-2021.07-x86_64-aarch64-non
 cp /usr/local/arm-cross-compiler/install/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 $ROOTFS/lib64
 cp /usr/local/arm-cross-compiler/install/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 $ROOTFS/lib64
 
-# # TODO: Make device nodes
-# echo a| sudo -S mknod -m 666 dev/null c 1 3
-# echo a| sudo -S mknod -m 666 dev/tty c 5 1
+# TODO: Make device nodes
+echo a| sudo -S mknod -m 666 dev/null c 1 3
+echo a| sudo -S mknod -m 666 dev/tty c 5 1
 
-# # TODO: Clean and build the writer utility
-# cd $FINDER_APP_DIR
-# make clean
-# make CROSS_COMPILE=$CROSS_COMPILE"gcc"
-# cp ./writer $ROOTFS/home
-# # TODO: Copy the finder related scripts and executables to the /home directory
-# # on the target rootfs
-# cp ./finder.sh $ROOTFS/home
-# cp ./writer.sh $ROOTFS/home
-# cp ./finder-test.sh $ROOTFS/home
-# cp ./autorun-qemu.sh $ROOTFS/home
+# TODO: Clean and build the writer utility
+cd $FINDER_APP_DIR
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE"gcc"
+cp ./writer $ROOTFS/home
+# TODO: Copy the finder related scripts and executables to the /home directory
+# on the target rootfs
+cp ./finder.sh $ROOTFS/home
+cp ./writer.sh $ROOTFS/home
+cp ./finder-test.sh $ROOTFS/home
+cp ./autorun-qemu.sh $ROOTFS/home
 
-# mkdir -p $ROOTFS/home/conf
-# cp ./conf/assignment.txt $ROOTFS/home/conf
-# cp ./conf/username.txt $ROOTFS/home/conf
+mkdir -p $ROOTFS/home/conf
+cp ./conf/assignment.txt $ROOTFS/home/conf
+cp ./conf/username.txt $ROOTFS/home/conf
 
-# # TODO: Chown the root directory
-# echo a| sudo -S chown root:root $ROOTFS
-# # TODO: Create initramfs.cpio.gz
-# cd $ROOTFS
-# find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
-# cd ..
-# gzip -f initramfs.cpio
+# TODO: Chown the root directory
+echo a| sudo -S chown root:root $ROOTFS
+# TODO: Create initramfs.cpio.gz
+cd $ROOTFS
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ..
+gzip -f initramfs.cpio
