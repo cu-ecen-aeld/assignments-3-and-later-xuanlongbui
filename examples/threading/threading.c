54707e64eef92fcf12e8f5eaf4f1ab5cb45492af
xuanlongbui <xuanlong1372000@gmail.com>
2024-10-13 10:46:42 +0700

    demo

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,6 +15,25 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    // Sleep for wait_to_obtain_ms milliseconds
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    // Obtain the mutex
+    pthread_mutex_lock(thread_func_args->mutex);
+    printf("Mutex obtained by thread\n");
+
+    // Hold the mutex for wait_to_release_ms milliseconds
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    // Release the mutex
+    pthread_mutex_unlock(thread_func_args->mutex);
+    printf("Mutex released by thread\n");
+    thread_func_args->thread_complete_success = true;
+    // Free the dynamically allocated memory
+    // free(thread_func_args);
+    // pthread_exit(NULL);
     return thread_param;
 }
 
@@ -28,6 +48,23 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* thread_arg;
+    thread_arg = (struct thread_data *)malloc(1*sizeof(struct thread_data ));
+    if(thread_arg == NULL){
+        printf("Unable to allocate memory for thread data\n");
+        return false;
+    }
+    
+    thread_arg->mutex = mutex;
+    thread_arg->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_arg->wait_to_release_ms = wait_to_release_ms;
+    int error; 
+    error = pthread_create(thread, NULL, &threadfunc, (void *)thread_arg); 
+    if (error != 0) {
+        printf("\nThread can't be created : [%s]", strerror(error)); 
+        free(thread_arg);
+        return false;
+    }
+    return true;
 }
 
