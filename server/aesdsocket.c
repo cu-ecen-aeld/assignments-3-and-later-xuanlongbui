0e66b1570971b50e50268312c601671993c469ef
xuanlongbui <xuanlong1372000@gmail.com>
2024-12-29 14:19:37 +0700

    ass6-part1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,305 +1,164 @@
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <syslog.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <arpa/inet.h>
-#include <string.h>
+#include "aesdsocket.h"
 
-#define BUFFER_SIZE 1024
-#define PORT "9000"
-#define BACKLOG 5
-const char *file_path = "/var/tmp/aesdsocketdata";
-unsigned int should_exit = 0;
-int server_fd;
-int client_fd;
+static pthread_mutex_t lock; 
 
-void handle_signal(int sig)
-{
-    printf("Received SIGINT (Ctrl+C) or SIGTERM, terminating the program...\n");
-    // You can do clean-up here, or use exit() to terminate the program
-    should_exit = 1;
-
-    if (remove(file_path) == 0)
-    {
-        printf("File '%s' deleted successfully.\n", file_path);
-    }
-    else
-    {
-        perror("Error deleting file");
-    }
-
-    if (client_fd >= 0)
-    {
-        close(client_fd);
-    }
-    if (server_fd >= 0)
-    {
-        close(server_fd);
-    }
-
-}
-
-int main(int argc, char *argv[])
-{
-    for (int i = 0; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-d") == 0)
-        {
-            printf("Runs the aesdsocket application as a daemon \n");
-            pid_t p;
-            p = fork();
-            if (p < 0)
-            {
-                perror("Error forking");
-                return -1;
-            }
-            if (p > 0)
-            {
-                exit(0);
-            }
-        }
-        else
-        {
-        }
-    }
-    int  s;
-    char *buffer;
-    // ssize_t                  nread;
-    // socklen_t                peer_addrlen;
-    size_t buffer_size = BUFFER_SIZE;
-
-    struct addrinfo hints;
-    struct addrinfo *result, *rp;
-    // struct sockaddr_storage  peer_addr;
-    int fd;
-
+static int fd =0;
+int init_file(const char * file_path){
     // Open the file, create it if it doesn't exist, with read/write permissions
     fd = open(file_path, O_CREAT | O_RDWR, 0644);
     if (fd < 0)
     {
-        perror("Failed to open or create file");
         // exit(EXIT_FAILURE);
         return -1;
     }
     printf("File '%s' created or opened successfully.\n", file_path);
+    return 0;
+}
+int close_file(){
+        // Close the file
+    if (close(fd) < 0)
+    {
+        return -1;
+    }
+    return 0;
+}
+int mutex_init(){
+       if (pthread_mutex_init(&lock, NULL) != 0) { 
+        return -1; 
+    }
+    return 0;
+}
+void mutex_destroy(){
+        pthread_mutex_destroy(&lock); 
+}
+void write_ip_to_syslog(struct sockaddr_in in_sockaddr, char out_ip[])
+{
+    // Convert client IP address to string
+    inet_ntop(AF_INET, &in_sockaddr.sin_addr, out_ip, INET_ADDRSTRLEN);
+    syslog(LOG_INFO, "Accepted connection from %s", out_ip);
+    printf("Client connected with IP address: %s and port: %d\n",
+           out_ip, ntohs(in_sockaddr.sin_port));
+}
+
+void *message_handler(void *args)
+{
+    int * c_fd= (int *)args;
+    int client_fd = * c_fd;
+    ssize_t bytes_received = 0;
+    char *buffer;
+    size_t buffer_size = BUFFER_SIZE;
+    size_t total_received = 0;
     // Dynamically allocate memory for the buffer
     buffer = (char *)malloc(buffer_size);
     if (buffer == NULL)
     {
         perror("Failed to allocate memory");
-        return -1;
+        // return -1;
     }
 
-    if (signal(SIGINT, handle_signal) == SIG_ERR)
+    while (1)
     {
-        perror("Error registering SIGINT handler");
-        return -1;
-        // exit(1);
-    }
-    if (signal(SIGTERM, handle_signal) == SIG_ERR)
-    {
-        perror("Error registering SIGTERM handler");
-        return -1;
-        // exit(1);
-    }
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;     /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_STREAM; /* stream socket */
-    hints.ai_flags = AI_PASSIVE;     /* For wildcard IP address */
-    hints.ai_protocol = 0;           /* Any protocol */
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
-
-    openlog("SyslogSocket", LOG_PID | LOG_CONS, LOG_USER);
-    s = getaddrinfo(NULL, PORT, &hints, &result);
-    if (s != 0)
-    {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-        // exit(EXIT_FAILURE);
-        return -1;
-    }
-
-    /* getaddrinfo() returns a list of address structures.
-        Try each address until we successfully bind(2).
-        If socket(2) (or bind(2)) fails, we (close the socket
-        and) try the next address. */
-
-    for (rp = result; rp != NULL; rp = rp->ai_next)
-    {
-        server_fd = socket(rp->ai_family, rp->ai_socktype,
-                           rp->ai_protocol);
-        if (server_fd == -1)
-            continue;
-
-        if (bind(server_fd, rp->ai_addr, rp->ai_addrlen) == 0)
-            break; /* Success */
-
-        close(server_fd);
-    }
-
-    freeaddrinfo(result); /* No longer needed */
-
-    if (rp == NULL)
-    { /* No address succeeded */
-        fprintf(stderr, "Could not bind\n");
-        // exit(EXIT_FAILURE);
-        return -1;
-    }
-
-    /* Read datagrams and echo them back to sender. */
-    if (listen(server_fd, BACKLOG) < 0)
-    {
-        perror("Listen failed");
-        close(server_fd);
-        // exit(EXIT_FAILURE);
-        return -1;
-    }
-    struct sockaddr_in client_address;
-    socklen_t client_address_len = sizeof(client_address);
-
-    char client_ip[INET_ADDRSTRLEN];
-    // Determine the client's IP address
-
-    ssize_t total_received = 0;
-    ssize_t bytes_received = 0;
-    while (!should_exit)
-    {
-        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_address, &client_address_len)) < 0)
+        bytes_received = recv(client_fd, buffer + total_received, BUFFER_SIZE, 0);
+        if (bytes_received == -1)
         {
-            perror("Accept failed");
-            close(server_fd);
-            // exit(EXIT_FAILURE);
-            // return -1;
-            continue;
+            perror("Receive failed");
+            break;
         }
-        printf("Connection accepted\n");
-        // Convert client IP address to string
-        inet_ntop(AF_INET, &client_address.sin_addr, client_ip, INET_ADDRSTRLEN);
-        syslog(LOG_INFO, "Accepted connection from %s", client_ip);
-        printf("Client connected with IP address: %s and port: %d\n",
-               client_ip, ntohs(client_address.sin_port));
-
-        while (1)
+        else if (bytes_received == 0)
         {
-            bytes_received = recv(client_fd, buffer + total_received, BUFFER_SIZE, 0);
-            if (bytes_received == -1)
+            printf("Client %d disconnected.\n", client_fd);
+            break;
+        }
+
+        total_received += bytes_received;
+
+        // Check if more space is needed in the buffer
+        if (total_received + BUFFER_SIZE > buffer_size)
+        {
+            buffer_size += BUFFER_SIZE; // Double the buffer size
+            buffer = (char *)realloc(buffer, buffer_size);
+            if (buffer == NULL)
             {
-                perror("Receive failed");
+                perror("Failed to reallocate memory");
+                close(client_fd);
+                // exit(EXIT_FAILURE);
+                // return -1;
+            }
+        }
+
+        // Stop receiving if the client sends less than a chunk
+        if (buffer[total_received - 1] == '\n')
+        {
+            pthread_mutex_lock(&lock); 
+            
+            printf("Received: %s", buffer);
+            ssize_t bytes_written = write(fd, buffer, total_received); // -1 to exclude null terminator
+            if (bytes_written < 0)
+            {
+                perror("Failed to write to file");
+                close(fd);
+                // return -1;
+            }
+            printf("Wrote %ld bytes .\n", bytes_written);
+            // Check for "exit" command to break the loop
+            // Read from the file until EOF is reached
+            ssize_t bytesRead = 0;
+            char *rbuffer = NULL; // Buffer to store data from the file
+            off_t file_size = 0;
+
+            // Get the size of the file
+            file_size = lseek(fd, 0, SEEK_END);
+            if (file_size == -1)
+            {
+                perror("Error getting file size");
+                close(fd);
+                // return 1;
                 break;
             }
-            else if (bytes_received == 0)
+            // Move the file pointer back to the beginning of the file
+            lseek(fd, 0, SEEK_SET);
+            // Allocate memory for the file content
+            rbuffer = (char *)malloc(file_size + 1); // +1 for null-terminator
+            if (rbuffer == NULL)
             {
-                // printf("Client disconnected.\n");
+                perror("Memory allocation error");
                 break;
+                // return -1;
             }
-
-            total_received += bytes_received;
-
-            // Check if more space is needed in the buffer
-            if (total_received + BUFFER_SIZE > buffer_size)
+            // Read the file content into the buffer
+            bytesRead = read(fd, rbuffer, file_size);
+            if (bytesRead == -1)
             {
-                buffer_size += BUFFER_SIZE; // Double the buffer size
-                buffer = (char *)realloc(buffer, buffer_size);
-                if (buffer == NULL)
-                {
-                    perror("Failed to reallocate memory");
-                    close(client_fd);
-                    close(server_fd);
-                    // exit(EXIT_FAILURE);
-                    return -1;
-                }
-            }
-
-            // Stop receiving if the client sends less than a chunk
-            if (buffer[total_received - 1] == '\n')
-            {
-                printf("Received: %s", buffer);
-                ssize_t bytes_written = write(fd, buffer, total_received); // -1 to exclude null terminator
-                if (bytes_written < 0)
-                {
-                    perror("Failed to write to file");
-                    close(fd);
-                    // exit(EXIT_FAILURE);
-                    return -1;
-                }
-                printf("Wrote %ld bytes to '%s'.\n", bytes_written, file_path);
-                // Check for "exit" command to break the loop
-                // Read from the file until EOF is reached
-                ssize_t bytesRead = 0;
-                char *rbuffer = NULL; // Buffer to store data from the file
-                off_t file_size = 0;
-
-                // Get the size of the file
-                file_size = lseek(fd, 0, SEEK_END);
-                if (file_size == -1)
-                {
-                    perror("Error getting file size");
-                    close(fd);
-                    return 1;
-                }
-                // Move the file pointer back to the beginning of the file
-                lseek(fd, 0, SEEK_SET);
-                // Allocate memory for the file content
-                rbuffer = (char *)malloc(file_size + 1); // +1 for null-terminator
-                if (rbuffer == NULL)
-                {
-                    perror("Memory allocation error");
-                    close(fd);
-                    return -1;
-                }
-                // Read the file content into the buffer
-                bytesRead = read(fd, rbuffer, file_size);
-                if (bytesRead == -1)
-                {
-                    perror("Error reading file");
-                    free(rbuffer);
-                    close(fd);
-                    return -1;
-                }
-
-                // Null-terminate the buffer to make it a string (optional, if you plan to treat it as text)
-                rbuffer[bytesRead] = '\0';
-                ssize_t bytesSent = send(client_fd, rbuffer, bytesRead, 0);
-                if (bytesSent == -1)
-                {
-                    perror("Error sending file data");
-                    close(fd);
-                    close(client_fd);
-                    close(server_fd);
-                    free(rbuffer);
-                    return -1;
-                }
-
+                perror("Error reading file");
                 free(rbuffer);
                 break;
+                // return -1;
             }
-        }
 
-        total_received = 0;
-        free(buffer);
-        buffer_size = BUFFER_SIZE;
-        buffer = (char *)malloc(buffer_size);
+            pthread_mutex_unlock(&lock); 
+            // Null-terminate the buffer to make it a string (optional, if you plan to treat it as text)
+            rbuffer[bytesRead] = '\0';
+            ssize_t bytesSent = send(client_fd, rbuffer, bytesRead, 0);
+            if (bytesSent == -1)
+            {
+                perror("Error sending file data");
+                close(client_fd);
+                free(rbuffer);
+                // return -1;
+                break;
+            }
+
+            free(rbuffer);
+            free(buffer);
+            total_received = 0;
+            buffer_size = BUFFER_SIZE;
+            buffer = (char *)malloc(buffer_size);
+        }
     }
     free(buffer);
-    // Write some initial data to the file
-
-    // Close the file
-    if (close(fd) < 0)
+    if (client_fd >= 0)
     {
-        perror("Failed to close file");
-        // exit(EXIT_FAILURE);
-        return 1;
+        close(client_fd);
     }
-
-    printf("File '%s' closed successfully.\n", file_path);
-    closelog();
-    return 0;
+    return NULL;
 }
